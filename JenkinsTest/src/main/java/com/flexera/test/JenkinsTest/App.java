d870c92f340ad4109ae06b6dc2d160719077f430
Saravanan Sivaji <SSivaji@flexera.com>
2019-05-02 11:44:12 +0530

    Initial

---
+++ JenkinsTest/src/main/java/com/flexera/test/JenkinsTest/App.java
@@ -0,0 +1,17 @@
+package com.flexera.test.JenkinsTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(createMessage());
+    }
+    
+    public static String createMessage() {
+    	return "Hello World!";
+    }
+}
